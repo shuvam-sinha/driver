2b0601c9412b3be603fda21018816976853b6b8c
Shuvam Sinha <shuvam.sinha4@gmail.com>
2022-12-28 11:17:09 -0800

    Filters based on Periods of Time

--- utils/data_reader.py
+++ utils/data_reader.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import csv
 import os
+import time
 
 
 seconds_per_day = 86400
@@ -23,14 +24,37 @@ def readRecords(filename1):
 #     return desired_rows
 
 
-def getAnalytics(userID, allRows, start_time = None, end_time = None):
+def getAnalytics(userID, allRows, duration = None, start_hour = None, end_hour = None):
+    # Get the list of filtered_rows based on start_time and end_time
+    start_time = 0
+    end_time = time.time()
+    filteredRows = []
+    if duration is None:
+        filteredRows = allRows
+    else:
+        if duration.lower() == "a":
+            start_time = end_time - 86400
+        elif duration.lower() == "b":
+            start_time = end_time - 604800
+        elif duration.lower() == "c":
+            start_time = end_time - 2628288
+        elif duration.lower() == "d":
+            start_time = end_time - 31536000
+    
+        for row in allRows:
+            cond1 = int(row['start_time']) >= start_time and int(row['end_time']) <= end_time
+            cond2 = int(row['start_time']) <= start_time and int(row['end_time']) >= start_time
+            cond3 = int(row['start_time']) <= end_time and int(row['end_time']) >= end_time
+            if cond1 or cond2 or cond3:
+                filteredRows.append(row)
+    
     desired_rows = []
-    for row in allRows:
+    for row in filteredRows:
         if row["user_id"] == userID:
-            if start_time is not None and end_time is not None:
-                cond1 = start_time >= int(row['start_time']) % seconds_per_day and start_time <= int(row['end_time']) % seconds_per_day
-                cond2 = end_time >= int(row['start_time']) % seconds_per_day and end_time <= int(row['end_time']) % seconds_per_day
-                cond3 = start_time < int(row['start_time']) % seconds_per_day and end_time > int(row['end_time']) % seconds_per_day
+            if start_hour is not None and end_hour is not None:
+                cond1 = start_hour >= int(row['start_time']) % seconds_per_day and start_hour <= int(row['end_time']) % seconds_per_day
+                cond2 = end_hour >= int(row['start_time']) % seconds_per_day and end_hour <= int(row['end_time']) % seconds_per_day
+                cond3 = start_hour < int(row['start_time']) % seconds_per_day and end_hour > int(row['end_time']) % seconds_per_day
                 cond = cond1 or cond2 or cond3
             else:
                 cond = True
@@ -56,6 +80,7 @@ if __name__ == "__main__":
 
     # Analytics across all time
     filteredAllRows = getAnalytics("shuvam_sinha", rows)
+    print("All Data")
     for row in filteredAllRows:
         print (row["user_id"], row["name"], row["start_time"], row["end_time"], row["num_short"], row["num_medium"], row["num_long"])
     (totalShort, totalMedium, totalLong) = analyzeResults(filteredAllRows)
@@ -64,10 +89,11 @@ if __name__ == "__main__":
     # Analytics within a time range
     filteredTimeRows = []
     for i in range(0, 24):
-        filteredTimeRows.append(getAnalytics("shuvam_sinha", rows, i*3600, (i+1)*3600))
+        filteredTimeRows.append(getAnalytics("shuvam_sinha", rows, "B", i*3600, (i+1)*3600-1))
 
+    print("Previous Week:")
     for iteration, thisTime in enumerate(filteredTimeRows):
-        print("Data: ", iteration)
+        print("Hour: ", iteration)
         for row in thisTime:
             print (row["user_id"], row["name"], row["start_time"], row["end_time"], row["num_short"], row["num_medium"], row["num_long"])
         (totalShort, totalMedium, totalLong) = analyzeResults(thisTime)
